37d43c6d1812237db2c0e69c781c2c18c3ec09bd
Tima1278 <104497043+Tima1278@users.noreply.github.com>
2024-05-09 09:57:40 +0000

    save

--- radioVue-main/radio/src/views/HomeView.vue
+++ radioVue-main/radio/src/views/HomeView.vue
@@ -2,54 +2,41 @@
   <v-container class="container-background">
     <h1 class="text-center">Radio</h1>
     <v-row>
-      <!-- Utilizzo v-for per creare righe con tre card ciascuna -->
       <v-col v-for="(radio, index) in radios" :key="index" cols="12" sm="4" md="4" lg="4">
         <v-card class="radio-card punk-card">
-  <!-- Aggiunta del container per il pulsante -->
-  <div class="btn-container">
-    <!-- Cuore per aggiungere ai preferiti -->
-    <v-btn @click="toggleFavorite(radio)" class="ma-2" color="transparent">
-      <v-icon :color="radio.isFavorite ? 'red' : 'white'" size="24">mdi-heart</v-icon>
-    </v-btn>
-  </div>
+          <div class="btn-container">
+            <v-btn @click="toggleFavorite(radio)" class="ma-2" color="transparent">
+              <v-icon :color="radio.isFavorite ? 'red' : 'white'" size="24">mdi-heart</v-icon>
+            </v-btn>
+          </div>
 
-  <!-- Foto della radio -->
-  <v-row justify="center">
-    <v-col cols="12" class="text-center">
-      <v-img :src="radio.favicon ? radio.favicon : require('@/assets/no immage.png')" height="200" contain></v-img>
-    </v-col>
-  </v-row>
+          <v-row justify="center">
+            <v-col cols="12" class="text-center">
+              <v-img :src="radio.favicon ? radio.favicon : require('@/assets/no immage.png')" height="200" contain></v-img>
+            </v-col>
+          </v-row>
 
-  <!-- Titolo della radio -->
-  <v-card-title class="text-center radio-title">{{ radio.name }}</v-card-title>
-  <v-card-actions class="d-flex justify-center">
-    <!-- Icona per riprodurre o mettere in pausa la radio -->
-    <v-btn @click="playRadio(radio)" color="transparent" class="ma-2">
-  <v-avatar class="play-button-container" color="transparent" tile>
-    <!-- Usa la condizione per visualizzare l'icona di play o pausa -->
-    <font-awesome-icon :icon="radio.isPlaying ? 'pause' : 'play'" style="color: white;" />
-  </v-avatar>
-</v-btn>
-
-
-    <!-- GIF di caricamento -->
-    <img v-if="isPlaying(radio)" src="@/assets/soundwave.gif" class="loading-gif" alt="Caricamento GIF">
-  </v-card-actions>
-</v-card>
-
+          <v-card-title class="text-center radio-title">{{ radio.name }}</v-card-title>
+          <v-card-actions class="d-flex justify-center">
+            <v-btn @click="playRadio(radio)" color="transparent" class="ma-2">
+              <div class="play-button-container">
+                <font-awesome-icon :icon="radio.isPlaying ? 'pause' : 'play'" style="color: white;" />
+              </div>
+            </v-btn>
+            <img v-if="isPlaying(radio)" src="@/assets/soundwave.gif" class="loading-gif" alt="Caricamento GIF">
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-// Importa Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
 import Hls from 'hls.js';
 
-// Aggiungi icone al core di Font Awesome
 library.add(faPlay, faPause);
 
 export default {
@@ -74,7 +61,7 @@ export default {
         .then(data => {
           this.radios = data.map(radio => ({
             ...radio,
-            isFavorite: false // Inizializza la proprietà isFavorite a false per ogni radio
+            isFavorite: false
           }));
         })
         .catch(error => {
@@ -82,42 +69,55 @@ export default {
         });
     },
     playRadio(radio) {
-      if (!this.audio) {
-        console.error('L\'oggetto audio non è stato inizializzato correttamente.');
-        return;
-      }
-      if (this.currentPlayingRadio && this.currentPlayingRadio !== radio) {
-        if (this.audio.hls) {
-          this.audio.hls.destroy();
-        }
-        this.audio.pause();
-      }
-      if (radio.url.endsWith('.m3u8')) {
-        if (Hls.isSupported()) {
-          this.audio.hls = new Hls();
-          this.audio.hls.loadSource(radio.url);
-          this.audio.hls.attachMedia(this.audio);
-          this.audio.addEventListener('canplaythrough', () => {
-            this.audio.play();
-            this.currentPlayingRadio = radio;
-          }, false);
-        } else if (this.audio.canPlayType('application/x-mpegURL')) {
-          this.audio.src = radio.url;
-          this.audio.addEventListener('canplaythrough', () => {
-            this.audio.play();
-            this.currentPlayingRadio = radio;
-          }, false);
-        } else {
-          console.error('Il browser non supporta la riproduzione di file m3u8.');
-        }
-      } else {
+  if (!this.audio) {
+    console.error('L\'oggetto audio non è stato inizializzato correttamente.');
+    return;
+  }
+  if (this.currentPlayingRadio && this.currentPlayingRadio !== radio) {
+    if (this.audio.hls) {
+      this.audio.hls.destroy();
+    }
+    this.audio.pause();
+    // Reset the icon of the previously playing radio to play
+    this.currentPlayingRadio.isPlaying = false;
+  }
+  if (this.isPlaying(radio)) {
+    // If the same radio is clicked again, pause it
+    this.audio.pause();
+    radio.isPlaying = false; // Set isPlaying to false
+    this.currentPlayingRadio = null;
+  } else {
+    // Start playing the selected radio
+    if (radio.url.endsWith('.m3u8')) {
+      if (Hls.isSupported()) {
+        this.audio.hls = new Hls();
+        this.audio.hls.loadSource(radio.url);
+        this.audio.hls.attachMedia(this.audio);
+        this.audio.addEventListener('canplaythrough', () => {
+          this.audio.play();
+          radio.isPlaying = true; // Set isPlaying to true
+          this.currentPlayingRadio = radio;
+        }, false);
+      } else if (this.audio.canPlayType('application/x-mpegURL')) {
         this.audio.src = radio.url;
         this.audio.addEventListener('canplaythrough', () => {
           this.audio.play();
+          radio.isPlaying = true; // Set isPlaying to true
           this.currentPlayingRadio = radio;
         }, false);
+      } else {
+        console.error('Il browser non supporta la riproduzione di file m3u8.');
       }
-    },
+    } else {
+      this.audio.src = radio.url;
+      this.audio.addEventListener('canplaythrough', () => {
+        this.audio.play();
+        radio.isPlaying = true; // Set isPlaying to true
+        this.currentPlayingRadio = radio;
+      }, false);
+    }
+  }
+},
     isPlaying(radio) {
       return this.currentPlayingRadio === radio;
     },
@@ -132,46 +132,47 @@ export default {
     FontAwesomeIcon
   },
   mounted() {
-    // Aggiungi icone al core di Font Awesome
     library.add(faPlay, faPause);
   }
 }
 </script>
 
 <style scoped>
-/* Stili personalizzati */
 .radio-card {
   margin-bottom: 20px;
   position: relative;
-  border: 2px solid #1976D2; /* Colore blu per il bordo */
-  border-radius: 10px; /* Arrotonda i bordi */
+  border: 2px solid #1976D2;
+  border-radius: 10px;
   display: flex;
-  flex-direction: column; /* Allineamento verticale dei contenuti */
-  justify-content: space-between; /* Spazio tra i contenuti */
-  height: 320px; /* Altezza fissa per rendere le card quadrate */
+  flex-direction: column;
+  justify-content: space-between;
+  height: 350px; /* Aumenta l'altezza di 10 pixel */
 }
 
+
 .loading-gif {
-  width: 170px; /* Imposta la larghezza della GIF */
-  height: auto; /* Imposta l'altezza automaticamente */
+  width: 170px;
+  height: auto;
 }
+
 .punk-card {
-  background: linear-gradient(45deg, #ff69b4, #8a2be2); /* Viola e Rosa */
+  background: linear-gradient(45deg, #ff69b4, #8a2be2);
 }
 
 .radio-title {
-  font-size: 24px; /* Imposta la dimensione del carattere */
-  font-weight: bold; /* Imposta il grassetto */
-  color: #333; /* Cambia il colore del testo */
-  text-transform: uppercase; /* Trasforma il testo in maiuscolo */
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+  text-transform: uppercase;
 }
 
 .btn-container {
   position: absolute;
-  top: 5px; /* Aggiunge spazio in alto */
-  right: 5px; /* Aggiunge spazio a destra */
-  z-index: 1; /* Imposta l'indice z */
+  top: 5px;
+  right: 5px;
+  z-index: 1;
 }
+
 .play-button-container {
   width: 40px;
   height: 40px;
@@ -179,15 +180,16 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  transition: background-color 0.3s; /* Aggiunge una transizione per il cambio di colore */
+  background-color: transparent;
+  transition: background-color 0.3s;
+  cursor: pointer;
 }
 
 .play-button-container:hover {
-  background-color: rgba(255, 255, 255, 0.2); /* Cambia il colore di sfondo al passaggio del mouse */
+  background-color: rgba(255, 255, 255, 0.2);
 }
 
 .play-button-container:active {
-  background-color: rgba(255, 255, 255, 0.4); /* Cambia il colore di sfondo quando premuto */
+  background-color: rgba(255, 255, 255, 0.4);
 }
-
 </style>
